98a9c215c6fbfad4b72fe0958e2a6222fec71871
Loek Le Blansch <l.leblansch@gmail.com>
2020-09-05 09:48:06 +0200

    betere witregels en poppetje

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import re
 import os
 import random
+import character
 
 def loadWords(): # laad woorden uit de ./words map en voeg ze toe aan een dictionary
     out = dict()
@@ -44,12 +45,15 @@ def main():
 
     # hoofdgedeelte
     word = getRandomWord(allWords).lower()
-    moves = 8
+    moves = 9
     guessedCharacters = set()
     print(f"Ik heb een woord in gedachten van {len(word)} letters", end="\n"*2)
     while moves > 0:
-        print(f"\n\t{formatWord(word, guessedCharacters)}\nJe kunt nog {moves} keer raden\nDe letters die je al geraden hebt zijn: {', '.join(guessedCharacters)}")
+        print(f"\t{formatWord(word, guessedCharacters)}\nJe kunt nog {moves} keer raden")
+        if len(guessedCharacters) > 0: print(f"De letters die je al geraden hebt zijn: {', '.join(guessedCharacters)}")
+        print(character.character[::-1][moves - 1])
         guess = input("Raad een letter of woord: ").lower()
+        print("\n"*2) # witregels
         if len(guess) > 1:
             # als de gok langer is dan 1 letter is het automatisch een gok voor een woord
             if word == guess:
@@ -65,6 +69,7 @@ def main():
             else:
                 guessedCharacters.add(guess)
                 print(f"De letter {guess} zit{' niet' if not guess in word else ''} in mijn woord")
+                if guess in word: continue
         else:
             # geen antwoord
             print("Je moet wel een letter of woord gokken")
